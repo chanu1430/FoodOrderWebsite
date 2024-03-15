60185ca6ac35562b26c0ca75a1323b230368a906
chanu1430 <73156448+chanu1430@users.noreply.github.com>
2024-03-15 20:39:35 +0530

    Update MiddleContent.js

--- frontEnd/src/MiddleContent.js
+++ frontEnd/src/MiddleContent.js
@@ -111,7 +111,18 @@ export default function MiddleContent() {
         }
         </div>
          <div className='col-lg-1 col-0'></div>
-    </div>:<></>
+    </div>:  <div id="outer-div-loading">
+      <div class="wrapper">
+          <div class="box-wrap">
+              <div class="box one"></div>
+              <div class="box two"></div>
+              <div class="box three"></div>
+              <div class="box four"></div>
+              <div class="box five"></div>
+              <div class="box six"></div>
+          </div>
+      </div>
+    </div>
   )
 }
 
